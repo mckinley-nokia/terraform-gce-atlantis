ec444d8fb5219c165cea1bfecc333ae090dcf2c6
Sean McKinley <sean.mckinley@nokia.com>
2024-09-26 10:21:40 -0600

    Christ alive

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">=6.2.0"
+      version = ">=4.79.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
